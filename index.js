36a46f8df0049da3504e7e6c0741e408b9db9a10
manendrarawat <manendra.rawat@oracle.com>
2024-03-24 19:34:04 +0530

    updated messages

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Test Messages'); //write a response to the client
+  res.write('Test Messages 123'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 4000
