e086b46ebaaa4a96671e78a63ca4662362a5f28e
manendrarawat <manendra.rawat@oracle.com>
2024-03-24 19:20:24 +0530

    updated messages

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('You are  in AWS docker  Cloud. THis is TECHESSAY, Please share and Subscribe and like my Videosaa'); //write a response to the client
+  res.write('You are  in AWS docker  Cloud. THis is TECHESSAY, Please share and Subscribe and like my Videosaabb'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 4000
